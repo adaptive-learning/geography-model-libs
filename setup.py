d4d737dcaa1f7beadca217cb2b9e8595e6323d31
Jan Papousek <jan.papousek@gmail.com>
2014-04-24 20:02:59 +0200

    use proso.geography as a namespace package

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     description='Models library for PROSO Geography project',
     author='Jan Papousek',
     author_email='jan.papousek@gmail.com',
+    namespace_packages = ['proso.geography', 'proso'],
     packages=['proso.geography', 'proso'],
     license='Gnu GPL v3',
     url='https://github.com/proso/geography-model-libs/',
