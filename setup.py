3546333ec2115dff668eeb4db72116398b801761
Jan Papousek <jan.papousek@gmail.com>
2014-04-24 16:01:56 +0200

    models renamed to model

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ VERSION = '0.9.7'
 
 
 setup(
-    name='proso-geography-models',
+    name='proso-geography-model',
     version=VERSION,
     description='Models library for PROSO Geography project',
     author='Jan Papousek',
     author_email='jan.papousek@gmail.com',
     packages=['proso.geography', 'proso'],
     license='Gnu GPL v3',
-    url='https://github.com/proso/geography-models-libs/',
+    url='https://github.com/proso/geography-model-libs/',
 )
