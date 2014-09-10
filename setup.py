b507aa4b8979dc1f4f691f68184e82fa22b8caa8
Jan Papousek <jan.papousek@gmail.com>
2014-09-10 09:01:40 +0200

    be more tolerant for dependencies

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email='jan.papousek@gmail.com',
     namespace_packages = ['proso.geography', 'proso'],
     packages=['proso.geography', 'proso'],
-    install_requires=['numpy==1.8.1', 'scikit-learn==0.15.0', 'matplotlib==1.3.1', 'clint==0.3.7', 'scipy==0.14.0'],
+    install_requires=['numpy>=1.8.1', 'scikit-learn>=0.15.0', 'matplotlib>=1.3.1', 'clint==0.3.7', 'scipy>=0.14.0'],
     license='Gnu GPL v3',
     url='https://github.com/proso/geography-model-libs/',
 )
